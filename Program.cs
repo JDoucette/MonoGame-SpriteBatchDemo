a0bee58e4f1331f9b57665ae687460780d204884
Jason Doucette <jdoucette@gmail.com>
2022-07-15 20:28:34 -0700

    project creation

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Jason Allen Doucette
+// July 15, 2022
+// SpriteBatch Demo
+
+using System;
+
+namespace SpriteBatchDemo
+{
+#if WINDOWS || LINUX
+	/// <summary>
+	/// The main class.
+	/// </summary>
+	public static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			using (var game = new Game1())
+				game.Run();
+		}
+	}
+#endif
+}
